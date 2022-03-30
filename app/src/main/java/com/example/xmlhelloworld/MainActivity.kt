524116560afa0e8fe3d24e1b8403e268ba352781
“AutoMendesgit config --global user.email “automendes30@gmail.com <automendes30@gmail.com>
2022-03-30 15:22:07 +0100

    App Done

---
+++ app/src/main/java/com/example/xmlhelloworld/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.xmlhelloworld
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var isTranslated = false
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val textViewHello = findViewById<TextView>(R.id.textViewHello)
+        val buttonTranslate = findViewById<Button>(R.id.buttonTranslate)
+
+        buttonTranslate.setOnClickListener {
+            if (isTranslated) {
+                textViewHello.text = "Hello world!"
+            }else {
+                textViewHello.text = "Olá mundo!"
+            }
+            isTranslated = !isTranslated
+        }
+    }
+}
